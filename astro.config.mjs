e32eb18ff3e422ff2cf59a7752def233a2d05f28
Lianor1 <lianorkavya@gmail.com>
2026-08-07 05:45:59 -0500

    switch to static output, remove cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
-  },
-
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: false
-    }
-  })
+  }
 });
